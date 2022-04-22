41551006cff951be209fbb9c352482dbd7134ba0
tshering <tshering.denkar@powercoders.org>
2022-04-22 15:53:03 +0200

    file update

---
+++ week4/functions/first-function/script.js
@@ -0,0 +1,12 @@
+function add(a,b){
+  let sum = (a + b);
+  return sum;
+  //console.log(sum);
+  
+}
+//console.log(add(5,8));
+var x; 
+x=add(4,5); 
+console.log(x); 
+//var y= add(3,6);
+//console.log(y);
