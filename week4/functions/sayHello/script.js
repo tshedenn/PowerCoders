757c6effe8f2036f97ccbaa0d68ea3114b806391
tshering <tshering.denkar@powercoders.org>
2022-04-25 15:58:31 +0200

    file update

--- week4/functions/sayHello/script.js
+++ week4/functions/sayHello/script.js
@@ -1,3 +1,25 @@
-function getNameFunction(){
-  
+// function greet(name){
+//     alert(`Hello ${name}`);
+// }
+// greet(prompt("Type your name: "));
+
+// let userName= name => {
+//   alert(`Hi ${name}`);
+// }
+// userName(prompt("Type in your name: "));
+
+
+// let number= (num1,num2)=>{
+//   return num1+num2;
+// }
+// number(5,1);
+
+
+let userInput = prompt("Type in your name: ");
+let count = ()=>{
+
+    lengthOf = lengthOf.length;
+    alert(`You have entered a string of ${lengthOf} characters`);
 }
+
+count(userInput);
