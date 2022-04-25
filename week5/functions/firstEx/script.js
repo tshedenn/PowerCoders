9725a1b453a624c0231eec4003405b15478b23ca
tshering <tshering.denkar@powercoders.org>
2022-04-25 16:42:35 +0200

    new file for week5

---
+++ week5/functions/firstEx/script.js
@@ -0,0 +1,14 @@
+let compareNum = (num1,num2)=>{
+   if (num1>num2){
+     alert(`The greater number of ${num1} and ${num2} is ${num1}.`);
+   } 
+
+   else{
+     alert(`The smaller number of ${num1} and ${num2}  is ${num1} `);
+
+   }  
+}
+compareNum(15,10);
+
+
+
---
+++ week5/functions/language-ex/script.js
@@ -0,0 +1,20 @@
+let helloWorld = lang =>{
+  if(lang==="In"){
+    alert("Namastey");
+  }
+  else if (lang==="de") {
+    alert("Hallo, Welt");
+  } 
+  else if (lang==="dz"){
+    alert("Kuzu zangpo");
+  }
+  else{
+    alert("Hello, World");
+  }
+  
+} 
+
+// helloWorld("In");
+// helloWorld("de");
+// helloWorld("dz");
+helloWorld("po");
