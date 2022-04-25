324432d8f04b4d2773d0274990090b69437d2ac3
tshering <tshering.denkar@powercoders.org>
2022-04-25 14:59:12 +0200

    files modify

--- week4/functions/first-function/script.js
+++ week4/functions/first-function/script.js
@@ -1,12 +1,29 @@
+/*var y=add(5,2);
+console.log(y);
+
+
 function add(a,b){
+  //var z=add(10,0);
+  //console.log(z);
   let sum = (a + b);
   return sum;
-  //console.log(sum);
   
+  //console.log(sum); 
 }
 //console.log(add(5,8));
-var x; 
-x=add(4,5); 
+//var x; 
+var x=add(4,5); 
 console.log(x); 
 //var y= add(3,6);
-//console.log(y);
+//console.log(y);*/
+
+//var myfunction = function(a,b,c){
+  //return a+b+c;
+//}
+//myfunction(1,2,3);
+
+a => {
+  let x= 100;
+  console.log(x);
+}
+
