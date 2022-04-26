b5b987daa5b43ea337b202be2c97ff0a2f68ba03
tshering <tshering.denkar@powercoders.org>
2022-04-26 16:30:12 +0200

    file update

---
+++ week5/functions/multidimensionalArray/script.js
@@ -0,0 +1,10 @@
+// Multidimensional Array, an array in an array
+
+let timeSpent = [
+    ['work',9],
+    ['Eat',2],
+    ['commute',1],
+    ['watch TV',2],
+    ['sleep', 7],
+];
+console.log(`Your tiem spent on sleep is ${timeSpent[4][1]} hours`);
