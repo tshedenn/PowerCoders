7154796bcbb8fb7eb7f90f03b4dc09d4937a3e45
tshering <tshering.denkar@powercoders.org>
2022-04-26 11:34:39 +0200

    file modify

--- week5/functions/language-ex/script.js
+++ week5/functions/language-ex/script.js
@@ -1,20 +1,30 @@
-let helloWorld = lang =>{
-  if(lang==="In"){
-    alert("Namastey");
-  }
-  else if (lang==="de") {
-    alert("Hallo, Welt");
-  } 
-  else if (lang==="dz"){
-    alert("Kuzu zangpo");
-  }
-  else{
-    alert("Hello, World");
-  }
+let lang ={
+    "en":"Hello, World",
+     "de": ""
+
+}
+
+
+
+
+
+// let helloWorld = lang =>{
+//   if(lang==="In"){
+//     alert("Namastey");
+//   }
+//   else if (lang==="de") {
+//     alert("Hallo, Welt");
+//   } 
+//   else if (lang==="dz"){
+//     alert("Kuzu zangpo");
+//   }
+//   else{
+//     alert("Hello, World");
+//   }
   
-} 
+// } 
 
 // helloWorld("In");
 // helloWorld("de");
 // helloWorld("dz");
-helloWorld("po");
+// helloWorld("po");
