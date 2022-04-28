98cc40a5e99680776981db889d1379c7f5fbf125
tshering <tshering.denkar@powercoders.org>
2022-04-28 11:51:58 +0200

    files update

---
+++ week5/functions/MadLib/script.js
@@ -0,0 +1,19 @@
+// let convertArray =[];
+// convertArray[0]= prompt("Enter a number: ");
+// convertArray[1]= prompt("Enter your name: ");
+
+// alert(convertArray);
+
+
+let tellStory = () => {
+  let story=[];
+  story[0]=prompt("Enter a noun ");
+  story[1]=prompt("Enter a verb ");
+  story[2]=prompt("Enter a adverb ");
+  story[3]=prompt("Enter a adjective");
+  console.log(`It's a story about ${story[0]}, a long time ago there was a  `);
+}
+
+tellStory();
+
+
