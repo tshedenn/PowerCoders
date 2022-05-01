6d2225f8a4c847690c13f769aca164d00fcc6cd5
tshering <tshering.denkar@powercoders.org>
2022-05-01 15:28:38 +0200

    practice of forloop and function

---
+++ week5/functions/printing-quotes/script.js
@@ -0,0 +1,20 @@
+// let quoteAuthor = (quote, author) => {
+  //    quote = prompt("Enter your quote");
+  //    author = prompt("Enter the author");
+//    alert(`${author} says, ${quote}`);
+// }
+// quoteAuthor();
+
+// for(let i=0; i<5; i++){
+//   for (let j = i; j <5; j++) {
+//    document.write(`*`);   
+//   }
+//    document.write(`<br/>`);
+// }
+
+// for(let i=0; i<5; i++){
+//   for (let j = 0; j <=i; j++) {
+//    document.write(`*`);   
+//   }
+//    document.write(`<br/>`);
+// }
