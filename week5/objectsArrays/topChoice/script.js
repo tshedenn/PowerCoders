ecd3c661c2fe138c7f4a856f758d7d3447b876a2
tshering <tshering.denkar@powercoders.org>
2022-05-01 19:22:32 +0200

    function with an array and object practice

---
+++ week5/objectsArrays/topChoice/script.js
@@ -0,0 +1,11 @@
+let myTopChoices = () => {
+let colorChoices = ['royal blue', 'sexy red', 'baby pink'];
+for (let i=0;i<colorChoices.length;i++){
+  console.log(`My #${i+1} is ${colorChoices[i]}`); 
+}
+let myColorChoice = {
+  weight : 'light'
+}
+  console.log(myColorChoice.weight);
+}
+myTopChoices();
